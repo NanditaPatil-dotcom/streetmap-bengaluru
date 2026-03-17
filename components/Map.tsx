7bbda10d3bef0f160e5a0ccb88b907051584dfcf
NanditaPatil-dotcom <nanditanpatil14@gmail.com>
2026-03-17 22:20:45 +0530

    feat: load places on the homepage map and use emoji pin markers

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,27 +1,50 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+
+type Place = {
+  _id?: string;
+  name: string;
+  category: string;
+  area: string;
+  location: {
+    coordinates: [number, number];
+  };
+};
 
 const defaultPosition: [number, number] = [12.9716, 77.5946]; // Bengaluru
 
-export default function Map({ places }: { places: any[] }) {
+const markerIcon = L.divIcon({
+  className: "emoji-marker",
+  html: '<div style="font-size: 28px; line-height: 1;">📍</div>',
+  iconSize: [28, 28],
+  iconAnchor: [14, 28],
+  popupAnchor: [0, -28],
+});
+
+export default function Map({ places }: { places: Place[] }) {
   return (
     <MapContainer
       center={defaultPosition}
-      zoom={13}
+      scrollWheelZoom
       style={{ height: "100vh", width: "100%" }}
+      zoom={13}
+      zoomControl
     >
       <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {places.map((place, index) => ( //markers 
+      {places.map((place, index) => (
         <Marker
-          key={index}
+          key={place._id ?? `${place.name}-${index}`}
+          icon={markerIcon}
           position={[
-            place.location.coordinates[1], // lat
-            place.location.coordinates[0], // lng
+            place.location.coordinates[1],
+            place.location.coordinates[0],
           ]}
         >
           <Popup>
